4bf95d096d4bfed0820eb0cc98a26be7086600d6
Tostin <tostin737@gmail.com>
2021-09-24 14:43:39 -0300

    Cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './base/nav/nav.component';
+import { AsideComponent } from './base/aside/aside.component';
+import { FooterComponent } from './base/footer/footer.component';
+import { ProductoComponent } from './general/producto/producto.component';
+import { CategoriaComponent } from './general/categoria/categoria.component';
+import { LoginComponent } from './general/login/login.component';
+import { RegistrarseComponent } from './general/registrarse/registrarse.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AsideComponent,
+    FooterComponent,
+    ProductoComponent,
+    CategoriaComponent,
+    LoginComponent,
+    RegistrarseComponent
   ],
   imports: [
     BrowserModule
